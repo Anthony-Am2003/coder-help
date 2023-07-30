6d93683361695b728903213722e09721f91af4ec
Anthony-Am2003 <tonyaamb2003@gmail.com>
2023-07-30 16:23:45 -0500

    change api model and tecnologies

--- api/entrypoint.sh
+++
@@ -1,1 +0,0 @@
-uvicorn src.main:app --reload --port=8000 --host=0.0.0.0
--- api/src/utils/router.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from fastapi.responses import JSONResponse
-
-utils_router = APIRouter()
-
-@utils_router.get("/ping")
-def ping():
-    return JSONResponse(content={"success":True,"msg":"Pong!"})
