83eacd8ed447f0b1de5e65529b1fd24942bea74c
Anthony-Am2003 <tonyaamb2003@gmail.com>
2023-08-03 22:23:17 -0500

    controlle handler and route to login created

--- api/src/routes/controllers/post/loginUser.js
+++ api/src/routes/controllers/post/loginUser.js
@@ -1,9 +1,19 @@
+//Dependencies
+const bcrypt = require("bcrypt")
+
 //Requires
 const {User} = require("../../../db");
 
 //Functions to validate data
-const {getUserByEmail} = require("../../../utils/validations")
+const {getUserByEmail} = require("../../../utils/validations");
 
 module.exports = async(email, password) => {
+    if(!email || !password) throw new Error("information is missing");
+    const user = getUserByEmail(email);
+    if(user){
+        const passwordCorrect = bcrypt.compare(password, user.password);
+        if(!passwordCorrect) throw new Error("Invalid email or password");
+    };
+
 
 }
--- api/src/routes/handlers/post/loginUser.js
+++ api/src/routes/handlers/post/loginUser.js
@@ -4,7 +4,7 @@ const validateLogin = require("../../controllers/post/loginUser")
 module.exports = async(req, res)=>{
     try {
         const {email, password} = req.body;
-        const accesToken = validateLogin(email, password)
+        const accesToken = await validateLogin(email, password)
         res.status(200).json(accesToken)
     } catch (error) {
         res.status(404).send(error.message)
