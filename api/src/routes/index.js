55b0a242f35450359431dcd41bcbe745479147ea
Anthony-Am2003 <tonyaamb2003@gmail.com>
2023-08-05 23:53:27 -0500

    route activate account created

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,6 +2,7 @@
 const createNewUser = require("../routes/handlers/post/createUser");
 const userLogin = require("../routes/handlers/post/loginUser");
 const recoveryPassword = require("../routes/handlers/post/recoveryPassword");
+const activateAccount = require("../routes/handlers/post/activateAccount");
 // GET ROUTES
 const sendTokenRecovery = require("../routes/handlers/get/resetPassword");
 //DELETE ROUTES
@@ -15,6 +16,7 @@ const router = Router();
 router.post("/user/login", userLogin);
 router.post("/user/register", createNewUser);
 router.post("/user/recovery-password", recoveryPassword);
+router.post("/user/activate-account", activateAccount);
 
 router.get("/user/reset-password", sendTokenRecovery);
 
