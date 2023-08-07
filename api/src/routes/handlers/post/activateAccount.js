29976e1515301b6dfffe956b392fb1964b520e21
Anthony-Am2003 <tonyaamb2003@gmail.com>
2023-08-06 21:01:43 -0500

    controller activate account created

--- api/src/routes/handlers/post/activateAccount.js
+++ api/src/routes/handlers/post/activateAccount.js
@@ -3,7 +3,7 @@ const activateAccount = require("../../conrollers/post/activateAccount");
 module.exports = async(req, res) => {
     try {
         const {email, code} = req.query;
-        const activateOperation = activateAccount(email, code);
+        const activateOperation = await activateAccount(email, code);
         res.status(200).json(activateOperation);
         } catch (error) {
         res.status(404).send(error.message);
